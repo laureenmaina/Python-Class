8bae0886a5b7cee33e4684edacd5715cb7153c18
Laurine Maina <127749979+laureenmaina@users.noreply.github.com>
2023-07-12 14:08:02 +0300

    Update ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,24 @@
+num1 = 1
+num2 = 17
+
+if num1>num2:
+    print("num1 is greater than num2")
+else:
+    print("num2 is greater than num1")
+
+# elif statement
+a = 20
+b = 20
+
+if a>b:
+    print("a is greater")
+elif a<b:
+    print("b is greater")
+else:
+    print("a and b are equal")
+
+number = int(input("Enter number:"))
+if  number%2==0:
+    print("Number is even")
+else:
+    print("Number is odd")
